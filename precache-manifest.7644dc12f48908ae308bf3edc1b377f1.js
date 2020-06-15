f2b98b158017968615bc353ee510e04d95639ac0
onetyrion <diego.m.tapia.z@gmail.com>
2020-06-15 07:18:18 -0400

    Updates

---
+++ precache-manifest.7644dc12f48908ae308bf3edc1b377f1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f3b7d9ab56e0890366d7149d2a74845",
+    "url": "/Chancador_Primario/index.html"
+  },
+  {
+    "revision": "837eb93a71a6a5e85c61",
+    "url": "/Chancador_Primario/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "781df4c2bf1b09d1650b",
+    "url": "/Chancador_Primario/static/css/main.23eb9b45.chunk.css"
+  },
+  {
+    "revision": "837eb93a71a6a5e85c61",
+    "url": "/Chancador_Primario/static/js/2.51bcf971.chunk.js"
+  },
+  {
+    "revision": "e9b70ed94c92560626f6d789c6814c32",
+    "url": "/Chancador_Primario/static/js/2.51bcf971.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "781df4c2bf1b09d1650b",
+    "url": "/Chancador_Primario/static/js/main.4fcbdf2d.chunk.js"
+  },
+  {
+    "revision": "f7bdf0cfc9ea0dd44285692948c2d84f",
+    "url": "/Chancador_Primario/static/js/main.4fcbdf2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59b84f44c613e0554789",
+    "url": "/Chancador_Primario/static/js/runtime-main.c400029d.js"
+  },
+  {
+    "revision": "3a9e59c9b5c237d88294faff733c2bb7",
+    "url": "/Chancador_Primario/static/media/candelarialogo.3a9e59c9.png"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/Chancador_Primario/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "beda0a804b8cb7aa5b5190da03aba6f8",
+    "url": "/Chancador_Primario/static/media/sidebar-5.beda0a80.jpg"
+  }
+]);
