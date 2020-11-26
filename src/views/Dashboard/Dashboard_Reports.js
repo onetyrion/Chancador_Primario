0c917bea6c3a3f1a297c5fb6d25d23709a8e6326
onetyrion <diego.m.tapia.z@gmail.com>
2020-11-26 05:45:50 -0300

    Fix Commits

--- src/views/Dashboard/Dashboard_Reports.js
+++ src/views/Dashboard/Dashboard_Reports.js
@@ -1,15 +1,8 @@
 import React from "react";
-import Iframe from 'react-iframe'
 
 export default function DashboardReports(props) {
-    const  file_get_contents = (filename) =>{
-        fetch(filename).then((resp) => resp.text()).then(function(data) {
-            return data;
-        });
-    }
   return(
 <div>
-{/* <a href="http://onetyrion-pc/ReportServer/Pages/ReportViewer.aspx?%2fReporte_DashboardEv3%2fReportDashboard&rs:Command=Render" target="_main" > Click here for the Territory Sales Drilldown sample report </a> */}
 
 <embed width="100%" height="600px" src="http://onetyrion-pc/ReportServer/Pages/ReportViewer.aspx?%2fReporte_DashboardEv3%2fReportDashboard&rs:embed=true"></embed>
 
